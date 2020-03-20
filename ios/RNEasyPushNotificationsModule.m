368e8752e480b24a98fa4baba95a2f8726612011
NichAga <33664465+NichAga@users.noreply.github.com>
2020-03-20 10:18:42 +0100

    [IOS] Auto-indentation

--- ios/RNEasyPushNotificationsModule.m
+++ ios/RNEasyPushNotificationsModule.m
@@ -12,12 +12,12 @@ extern NSDictionary *remoteNotification = NULL;
 RCT_EXPORT_MODULE(BlitzNotifications);
 
 + (id)allocWithZone:(NSZone *)zone {
-  static RNEasyPushNotificationsModule *sharedInstance = nil;
-  static dispatch_once_t onceToken;
-  dispatch_once(&onceToken, ^{
-    sharedInstance = [super allocWithZone:zone];
-  });
-  return sharedInstance;
+    static RNEasyPushNotificationsModule *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [super allocWithZone:zone];
+    });
+    return sharedInstance;
 }
 
 - (NSArray<NSString *> *)supportedEvents {
@@ -44,53 +44,53 @@ RCT_EXPORT_METHOD(getLastNotificationData:(RCTResponseSenderBlock)callback)
 
 RCT_EXPORT_METHOD(registerForToken)
 {
-  dispatch_async(dispatch_get_main_queue(), ^{
-    
-      if(device_id == NULL){
-      FIRApp *firApp = [FIRApp defaultApp];
-     UIApplication *application = UIApplication.sharedApplication;
-    [FIRMessaging messaging].delegate = self;
-    [FIRMessaging messaging].shouldEstablishDirectChannel = YES;
-    
-   if ([UNUserNotificationCenter class] != nil) {
-     [UNUserNotificationCenter currentNotificationCenter].delegate = self;
-     UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert |
-         UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
-     [[UNUserNotificationCenter currentNotificationCenter]
-         requestAuthorizationWithOptions:authOptions
-         completionHandler:^(BOOL granted, NSError * _Nullable error) {
-          
-         }];
-   } else {
-     // iOS 10 notifications aren't available; fall back to iOS 8-9 notifications.
-   
-     UIUserNotificationType allNotificationTypes =
-     (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
-     UIUserNotificationSettings *settings =
-     [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
-     [application registerUserNotificationSettings:settings];
-   }
-
-   [application registerForRemoteNotifications];
-      } else {
-          [self sendEventWithName:@"deviceRegistered" body:device_id];
-      }
-     });
+    dispatch_async(dispatch_get_main_queue(), ^{
+        
+        if(device_id == NULL){
+            FIRApp *firApp = [FIRApp defaultApp];
+            UIApplication *application = UIApplication.sharedApplication;
+            [FIRMessaging messaging].delegate = self;
+            [FIRMessaging messaging].shouldEstablishDirectChannel = YES;
+            
+            if ([UNUserNotificationCenter class] != nil) {
+                [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+                UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert |
+                UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
+                [[UNUserNotificationCenter currentNotificationCenter]
+                 requestAuthorizationWithOptions:authOptions
+                 completionHandler:^(BOOL granted, NSError * _Nullable error) {
+                    
+                }];
+            } else {
+                // iOS 10 notifications aren't available; fall back to iOS 8-9 notifications.
+                
+                UIUserNotificationType allNotificationTypes =
+                (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
+                UIUserNotificationSettings *settings =
+                [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
+                [application registerUserNotificationSettings:settings];
+            }
+            
+            [application registerForRemoteNotifications];
+        } else {
+            [self sendEventWithName:@"deviceRegistered" body:device_id];
+        }
+    });
 }
 
 // [START receive_message]
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)notification {
-          remoteNotification = notification;
+    remoteNotification = notification;
     NSLog(@"notificationReceived didReceiveRemoteNotification : %@", remoteNotification);
-  [self sendEventWithName:@"notificationReceived" body: remoteNotification];
+    [self sendEventWithName:@"notificationReceived" body: remoteNotification];
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)notification
 fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
-      remoteNotification = notification;
-  NSLog(@"notificationReceived didReceiveRemoteNotification with completionhandler: %@", remoteNotification);
-  [self sendEventWithName:@"onNotificationTap" body: remoteNotification];
-  completionHandler(UIBackgroundFetchResultNewData);
+    remoteNotification = notification;
+    NSLog(@"notificationReceived didReceiveRemoteNotification with completionhandler: %@", remoteNotification);
+    [self sendEventWithName:@"onNotificationTap" body: remoteNotification];
+    completionHandler(UIBackgroundFetchResultNewData);
 }
 // [END receive_message]
 
@@ -100,34 +100,34 @@ fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center
        willPresentNotification:(UNNotification *)notification
          withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
-
-  completionHandler(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge);
-  NSLog(@"notificationReceived userNotificationCenter with UNNotificationPresentationOptions: %@", remoteNotification);
-  // when we reveive it in foreground
+    
+    completionHandler(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge);
+    NSLog(@"notificationReceived userNotificationCenter with UNNotificationPresentationOptions: %@", remoteNotification);
+    // when we reveive it in foreground
     remoteNotification = notification.request.content.userInfo;
-  [self sendEventWithName:@"notificationReceived" body: remoteNotification];
-  completionHandler(UNNotificationPresentationOptionNone);
+    [self sendEventWithName:@"notificationReceived" body: remoteNotification];
+    completionHandler(UNNotificationPresentationOptionNone);
 }
 
 // Handle notification messages after display notification is tapped by the user.
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center
 didReceiveNotificationResponse:(UNNotificationResponse *)notification
          withCompletionHandler:(void(^)(void))completionHandler {
-  /// when we tap on notif and app is in foreground
+    /// when we tap on notif and app is in foreground
     NSLog(@"notificationReceived didReceiveRemoteNotification with completionhandler: %@", notification);
-  remoteNotification = notification.notification.request.content.userInfo;
-  [self sendEventWithName:@"onNotificationTap" body: remoteNotification];
-  completionHandler();
+    remoteNotification = notification.notification.request.content.userInfo;
+    [self sendEventWithName:@"onNotificationTap" body: remoteNotification];
+    completionHandler();
 }
 
 // [END ios_10_message_handling]
 
 // [START refresh_token]
 - (void)messaging:(FIRMessaging *)messaging didReceiveRegistrationToken:(NSString *)fcmToken {
-  device_id = fcmToken;
+    device_id = fcmToken;
     NSLog(@"notificationReceived didReceiveMessage with device_id: %@", device_id);
-  [self sendEventWithName:@"deviceRegistered" body:fcmToken];
-  
+    [self sendEventWithName:@"deviceRegistered" body:fcmToken];
+    
 }
 // [END refresh_token]
 
@@ -135,9 +135,9 @@ didReceiveNotificationResponse:(UNNotificationResponse *)notification
 // Receive data messages on iOS 10+ directly from FCM (bypassing APNs) when the app is in the foreground.
 // To enable direct data messages, you can set [Messaging messaging].shouldEstablishDirectChannel to YES.
 - (void)messaging:(FIRMessaging *)messaging didReceiveMessage:(FIRMessagingRemoteMessage *)notification {
-  remoteNotification = notification;
-  NSLog(@"notificationReceived didReceiveMessage with didReceiveMessage: %@", remoteNotification);
-  [self sendEventWithName:@"notificationReceived" body: remoteNotification];
+    remoteNotification = notification;
+    NSLog(@"notificationReceived didReceiveMessage with didReceiveMessage: %@", remoteNotification);
+    [self sendEventWithName:@"notificationReceived" body: remoteNotification];
 }
 
 // [END ios_10_data_message]
@@ -154,15 +154,15 @@ didReceiveNotificationResponse:(UNNotificationResponse *)notification
 -(void) setRemoteNotification:(NSDictionary *) notification
 {   NSLog(@"setRemoteNotification %@",notification);
     remoteNotification = notification;
-      [self sendEventWithName:@"onNotificationTap" body: remoteNotification];
+    [self sendEventWithName:@"onNotificationTap" body: remoteNotification];
 }
 
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
-  NSLog(@"Unable to register for remote notifications: %@", error);
+    NSLog(@"Unable to register for remote notifications: %@", error);
 }
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-  NSLog(@"APNs device token retrieved: %@", deviceToken);
+    NSLog(@"APNs device token retrieved: %@", deviceToken);
     [self sendEventWithName:@"deviceRegistered" body:deviceToken];
 }
 @end
